86b3d70cf81637e106be34caba8b582f8adc9fbc
RS Shonjoy <85416231+rsshonjoydas@users.noreply.github.com>
2024-03-02 08:04:18 +0600

    feat: initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@/components/ui/button';
+
+export default function Home() {
+  return (
+    <section>
+      <h1 className='text-purple-500'>RS Shonjoy</h1>
+      <Button variant='ghost'>Redolence</Button>
+    </section>
+  );
+}
